18b781f356c3919a579bbe2c4cacf4b73902cd30
Parth Sarthi Mishra <wforsarthi_biness@outlook.com>
2026-01-02 11:51:58 +0530

    analyse the db

---
+++ dataAnalysis.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import sqlite3
+
+conn = sqlite3.connect('GideonLog.db')
+cursor = conn.cursor()
+
+# cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+# print(cursor.fetchall())
+
+df = pd.read_sql_query("SELECT * from  history", conn)
+
+print(df.head())
