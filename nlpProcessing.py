dad7838a19abbf0c2f42a44d1a1f50dbd1bb727a
Parth Sarthi Mishra <wforsarthi_biness@outlook.com>
2026-01-09 20:46:40 +0530

    planning

--- nlpProcessing.py
+++ nlpProcessing.py
@@ -6,5 +6,8 @@ conn = sqlite3.connect('GideonLog.db')
 df = pd.read_sql_query('SELECT raw_text FROM history', conn)
 
 #this stil doesn't make any sense 
-
+# things to do 
+# 1. understand the database for gideon , i.e. sqlite
+# 2. add more commands
+# 3. understand the ai and make it even robuts
 
